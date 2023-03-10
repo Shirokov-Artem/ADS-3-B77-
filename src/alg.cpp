8a58a3bca0e21d06bbd07870dea85e3cb6e66973
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-11 00:54:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,18 +1,13 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int a = 0;
-  int b = size - 1;
-  do {
-    int ab = (a + b) / 2;
-    if (value == arr[ab]) {
-      return ab;
-    } else if (value < arr[ab]) {
-      b = ab - 1;
-    } else {
-      a = ab + 1;
+  int quantity = 0;
+  for (int i = 0; i < size, ++i) {
+    if (value == arr[i]) {
+      quantity = quantity + 1;
     }
-  } while (a <= b);
+  }
+  return quantity;
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
 }
