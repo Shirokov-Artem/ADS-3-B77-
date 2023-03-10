5b708d2ac3e19a289e426792a502482a86ddcbe2
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-03-11 01:04:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,7 @@
 
 int cbinsearch(int *arr, int size, int value) {
   int quantity = 0;
-  for (int i = 0; i < size, ++i) {
+  for (int i = 0; i < size; ++i) {
     if (value == arr[i]) {
       quantity = quantity + 1;
     }
